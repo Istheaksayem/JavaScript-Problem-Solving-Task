0d07aa1a48362ba049dc5b14750b599094aeb856
Istheaksayem <istheakahmed9345@gmail.com>
2026-01-07 22:08:02 +0600

    added problem solving 2 in js

---
+++ Problem2/Problem2.js
@@ -0,0 +1,14 @@
+// Count Vowels
+
+
+function countVowels(str) {
+    let vowels ="aeiou";
+    let count =0;
+
+    for(let i =0; i <str.length; i++){
+        if(vowels.includes(str[i].toLowercase())){
+            count++;
+        }
+    }
+    return count;
+}
