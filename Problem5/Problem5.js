074941a10ff99af7f347a66b3e2cac3548de2e73
Istheaksayem <istheakahmed9345@gmail.com>
2026-01-08 09:11:52 +0600

    added problem find max number

---
+++ Problem5/Problem5.js
@@ -0,0 +1,12 @@
+//  Remove Duplicates
+function removeDuplicates(arr) {
+  let unique = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!unique.includes(arr[i])) {
+      unique.push(arr[i]);
+    }
+  }
+
+  return unique;
+}
